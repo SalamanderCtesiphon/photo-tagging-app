eac564a2f18b34ba3639529c568cbd5c01ac9241
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-18 12:56:40 -0500

    trying to deploy

--- src/index.js
+++ src/index.js
@@ -3,23 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBA9VGZM9yQQe1lMQ9jxHD7lRZX5owCCTM",
-  authDomain: "photo-tagging-app-438b7.firebaseapp.com",
-  projectId: "photo-tagging-app-438b7",
-  storageBucket: "photo-tagging-app-438b7.appspot.com",
-  messagingSenderId: "737957340387",
-  appId: "1:737957340387:web:00353f548551c5de0711d6"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
