ec5c31ea2d2d822bc02ac93e3511165914c00530
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-19 15:47:09 -0500

    removed a button and a function that was previously used to generate the grid overlay

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import React from 'react'
 function Header({makeDivs}) {
   return (
     <div className='header'>
-      <button onClick={() => makeDivs()}>start</button>
       <h1>Wheres Baldo?</h1>
     </div>
   )
