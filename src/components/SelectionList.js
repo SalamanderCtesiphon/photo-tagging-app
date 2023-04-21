a291070edeb48eefa3938dbc0c7828dd8ef22d61
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-21 12:30:45 -0500

    cleaned up the code a little

--- src/components/SelectionList.js
+++ src/components/SelectionList.js
@@ -7,24 +7,12 @@ function SelectionList() {
     <div className='drop-down'>
       { canidates.map((canidate) => {
         return (
-          <div className='image-card'>
+          <div className='image-card' key={canidate.id}>
             <img src={canidate.image} alt={`${canidate.title}`}></img>
             <p>{canidate.title}</p>
           </div>
         )
       })}
-      {/* <div className='image-card'>
-        <img src={babyWizard} alt='baby wizard'></img>
-        <p>Baby Wizard</p>
-      </div>
-      <div className='image-card'>
-        <img src={ghostInAJar} alt='ghost in a jar'></img>
-        <p>Ghost in a Jar</p>
-      </div>
-      <div className='image-card'>
-        <img src={pennsylvester} alt='pennsylvester'></img>
-        <p>Pennsylvester</p>
-      </div> */}
     </div>
   )
 }
