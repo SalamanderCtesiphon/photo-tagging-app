aab4f7475fd842773825bd65ccf05e9936301f91
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-21 12:14:43 -0500

    created a canidates array to hold the possible selections

---
+++ src/data/PossibilitiesArray.js
@@ -0,0 +1,23 @@
+import babyWizard from '../images/babyWizard.png'
+import ghostInAJar from '../images/ghostInAJar.png'
+import pennsylvester from '../images/pennsylvester.png'
+
+const canidates = [
+  {
+    title: 'babyWizard',
+    image: babyWizard,
+    id: 1
+  },
+  {
+    title: 'ghostInAJar',
+    image: ghostInAJar,
+    id: 2
+  },
+  {
+    title: 'pennsylvester',
+    image: pennsylvester,
+    id: 3
+  }
+]
+
+export default canidates
