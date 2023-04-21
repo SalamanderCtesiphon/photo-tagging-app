4f47499b7f25615aee8f96e67723bd48d2fcb50e
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-21 12:01:51 -0500

    added two more images to the selection drop down

--- src/components/SelectionList.js
+++ src/components/SelectionList.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import babyWizard from '../images/babyWizard.png'
+import ghostInAJar from '../images/ghostInAJar.png'
+import pennsylvester from '../images/pennsylvester.png'
 
 function SelectionList() {
   return (
     <div className='drop-down'>
-      <img src={babyWizard} alt='baby wizard'></img>
-      <p>Baby Wizard</p>
+      <div className='image-card'>
+        <img src={babyWizard} alt='baby wizard'></img>
+        <p>Baby Wizard</p>
+      </div>
+      <div className='image-card'>
+        <img src={ghostInAJar} alt='ghost in a jar'></img>
+        <p>Ghost in a Jar</p>
+      </div>
+      <div className='image-card'>
+        <img src={pennsylvester} alt='pennsylvester'></img>
+        <p>Pennsylvester</p>
+      </div>
     </div>
   )
 }
